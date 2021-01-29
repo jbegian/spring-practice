35e93a52b26ed92f2f1ebf341230ba8a5e6c22a8
Jordan Begian <begian.j@gmail.com>
2021-01-29 00:02:14 -0500

    update controller to use service

--- src/main/java/com/exercise/springpractice/controller/UserController.java
+++ src/main/java/com/exercise/springpractice/controller/UserController.java
@@ -3,8 +3,9 @@ package com.exercise.springpractice.controller;
 import java.util.List;
 
 import com.exercise.springpractice.model.User;
-import com.exercise.springpractice.repository.UserRepository;
+import com.exercise.springpractice.service.UserService;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,19 +16,19 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
 
-    private final UserRepository userRepository;
+    private final UserService userService;
 
-    public UserController(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
     @GetMapping("/users")
     public List<User> getUsers() {
-        return (List<User>) userRepository.findAll();
+        return userService.findAllUsers();
     }
 
     @PostMapping("/users")
-    public void addUser(@RequestBody User user) {
-        userRepository.save(user);
+    public ResponseEntity<User> addUser(@RequestBody User user) {
+        return ResponseEntity.ok(userService.createUser(user));
     }
 }
