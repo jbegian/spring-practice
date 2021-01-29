7c1e6f6bfc551350dbf2232d80369e58818f19d0
Jordan Begian <begian.j@gmail.com>
2021-01-29 00:02:25 -0500

    add user service

---
+++ src/main/java/com/exercise/springpractice/service/UserService.java
@@ -0,0 +1,23 @@
+package com.exercise.springpractice.service;
+
+import java.util.List;
+
+import com.exercise.springpractice.model.User;
+import com.exercise.springpractice.repository.UserRepository;
+
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    
+    public List<User> findAllUsers() {
+        return (List<User>) userRepository.findAll();
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+}
